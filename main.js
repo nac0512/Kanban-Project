7e465a91c092ee9108ca976bb20ac7410322a319
ConeyNicollete-FS <naclemons@student.fullsail.edu>
2021-08-13 14:18:34 -0700

    Created functionality to display form when new task button is clicked

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ class Main {
                     const article = document.createElement("article");
                     article.setAttribute("id", `${responseAsJson[i].items[j].id}`);
                     article.setAttribute("listID", `${responseAsJson[i].items[j].listId}`)
-                    article.innerHTML = `<h3>${responseAsJson[i].items[j].title}</h3><p>${responseAsJson[i].items[j].description}</p><time>${responseAsJson[i].items[j].dueDate}</time><div><button class="icon-edit">Edit</button><button class="icon-delete">Delete</button></div>`;
+                    article.innerHTML = `<h3>${responseAsJson[i].items[j].title}</h3><p>${responseAsJson[i].items[j].description}</p><time>${responseAsJson[i].items[j].dueDate}</time><div class="edits"><button class="icon-edit">Edit</button><button class="icon-delete">Delete</button></div>`;
                     document.querySelector(`section:nth-of-type(${i+1})`).appendChild(article);
 
                 } 
--- newtask.js
+++ newtask.js
@@ -1,19 +1,43 @@
 class NewTask {
     constructor() {
         document.querySelector(".icon-add").addEventListener("click", e=>this.create(e));
-        // document.querySelector("button[type='submit']").addEventListener("click", e=>this.submit(e));
-        // document.querySelector("#cancelSubmit").addEventListener("click", e=>this.cancel(e));
+        document.querySelector("button[type='submit']").addEventListener("click", e=>this.submit(e));
+        document.querySelector("button[type='button']").addEventListener("click", e=>this.cancel(e));
     } 
 
     create(e) {
-        
+        document.querySelectorAll("section").forEach(e => {
+            const option = document.createElement("option");
+            option.value = e.getAttribute("id");
+            option.text = e.firstChild.textContent;
+            document.querySelector("select").appendChild(option);
+        });
+
+        document.querySelector("#container").style.filter= "blur(10px)";
+        document.querySelector("form").style.display= "block";
     }
 
     submit(e) {
-        e.preventDefault;
+        e.preventDefault();
+
+        let check = document.querySelector("form").reportValidity();
+
+        if(check) {
+            
+
+        }
+        else {
+            throw "Fields mssing information";
+        }
     }
 
     cancel(e) {
+        document.querySelectorAll("option").forEach(e => {
+            e.remove();
+        })
 
+        document.querySelector("form").reset();
+        document.querySelector("#container").style.filter= "blur(0px)";
+        document.querySelector("form").style.display = "none";
     }
 }
